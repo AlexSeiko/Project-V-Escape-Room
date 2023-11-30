2186c353e1b15de18076dbe2af7e55e9ecbb457a
P1nta1nh0 <dmpinto10@gmail.com>
2023-11-30 09:11:08 +0000

    Buttons and physics

---
+++ Assets/Scripts/ButtonPuzzleManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPuzzleManager : MonoBehaviour
+{
+    [SerializeField] private List<ChestButtons> Buttons = new List<ChestButtons>();
+    [SerializeField] private List<EbuttonTypes> CorrectButtons = new List<EbuttonTypes>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Lets Find All Activated Buttons
+        List<ChestButtons> ActivatedButtons = new List<ChestButtons>();
+        for (int i = 0; i < Buttons.Count; i++)
+        {
+            var button = Buttons[i];
+            if (button != null)
+            {
+                if(button.IsPressed)
+                {
+                    ActivatedButtons.Add(button);
+                }
+            }
+        }
+
+        if(ActivatedButtons.Count == CorrectButtons.Count)
+        {
+            for (int i = 0; i < ActivatedButtons.Count; i++)
+            {
+                var button = ActivatedButtons[i];
+                if (button != null)
+                {
+                    bool IsCorrectButtonPressed = false;
+                    for (int j = 0; j < CorrectButtons.Count; j++)
+                    {
+                        if (button.ButtonType == CorrectButtons[j])
+                        {
+                            IsCorrectButtonPressed = true;
+                            break;
+                        }
+                    }
+
+                    if (!IsCorrectButtonPressed) return;
+                }
+            }
+
+            // Do Opening Of Chest Lid
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/ChestButtons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EbuttonTypes
+{
+    Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8
+}
+
+public class ChestButtons : MonoBehaviour
+{
+    [field: SerializeField]public EbuttonTypes ButtonType{ get; set; }
+
+    public bool IsPressed { get; set; }
+
+}
