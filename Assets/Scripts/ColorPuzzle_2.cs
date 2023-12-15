32453709ec85d9b7bf70624bbde074df0f6bc16e
P1nta1nh0 <dmpinto10@gmail.com>
2023-12-15 11:10:40 +0000

    Color Puzzle for the second corner

---
+++ Assets/Scripts/ColorPuzzle_2.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPuzzle_2 : MonoBehaviour
+{
+    [SerializeField] private GameObject Button1;
+    [SerializeField] private GameObject Button2;
+    [SerializeField] private GameObject Button3;
+    [SerializeField] private GameObject Button4;
+
+    [SerializeField] private int Button1RepeatEmission;
+    [SerializeField] private int Button2RepeatEmission;
+    [SerializeField] private int Button3RepeatEmission;
+    [SerializeField] private int Button4RepeatEmission;
+
+    private Material Button1Material;
+    private Material Button2Material;
+    private Material Button3Material;
+    private Material Button4Material;
+
+    private Coroutine SimonSaysCoroutine;
+
+    private void Start()
+    {
+        List<Material> materials = new List<Material>();
+        Button1.GetComponent<MeshRenderer>().GetMaterials(materials);
+        Button1Material = materials[0];
+
+        materials.Clear();
+        Button2.GetComponent<MeshRenderer>().GetMaterials(materials);
+        Button2Material = materials[0];
+
+        materials.Clear();
+        Button3.GetComponent<MeshRenderer>().GetMaterials(materials);
+        Button3Material = materials[0];
+
+        materials.Clear();
+        Button4.GetComponent<MeshRenderer>().GetMaterials(materials);
+        Button4Material = materials[0];
+
+        Interact();
+    }
+
+    public void Interact()
+    {
+        if (SimonSaysCoroutine != null)
+            return;
+
+        SimonSaysCoroutine = StartCoroutine(SimonSaysLightUpObjects());
+    }
+
+    private IEnumerator SimonSaysLightUpObjects()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for(int i = 0; i < Button1RepeatEmission; ++i)
+        {
+            Button1Material.EnableKeyword("_EMISSION");
+
+            yield return new WaitForSeconds(0.25f);
+
+            Button1Material.DisableKeyword("_EMISSION");
+
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        // Copy Paste For Loop In here
+
+
+    }
+}
