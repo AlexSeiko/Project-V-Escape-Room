c818b14dbd42550038e054ae6168ccf057ddb07c
Nelson Luis Moreira da Costa <nelson.costa@universidadeeuropeia.pt>
2023-12-27 22:45:38 +0000

    Made a small script to play an animation to open and close a door when you get close to it

---
+++ Assets/Scripts/OpenWhenInCloseProximity.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SphereCollider))]
+public class OpenWhenInCloseProximity : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            animator.Play("Open");
+        }
+    }
+    
+    private void OnTriggerExit(Collider Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            animator.Play("Close");
+        }
+    }
+}
