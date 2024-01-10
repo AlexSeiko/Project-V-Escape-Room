9829936701a5e2ee09706384f1d08e901b672449
P1nta1nh0 <dmpinto10@gmail.com>
2024-01-10 22:21:50 +0000

    Door changes, Change between Cinema and Exiteca teleporters

---
+++ Assets/Scripts/DeactivateCinemateca.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class DeactivateCinemateca : MonoBehaviour
+{
+    [SerializeField] private GameObject CinematecaComponent;
+    [SerializeField] private GameObject ExitecaComponent;
+    [SerializeField] private GameObject TeleporterShow;
+    [SerializeField] private GameObject TeleporterHide;
+
+    void Start()
+    {
+        GetComponent<SphereCollider>().isTrigger = true;
+
+        ExitecaComponent?.SetActive(false);
+        TeleporterShow?.SetActive(false);
+        TeleporterHide?.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the CinematecaComponent is not null before deactivating
+        if (CinematecaComponent != null)
+        {
+            CinematecaComponent.SetActive(false);
+        }
+
+        if (ExitecaComponent != null)
+        {
+            ExitecaComponent.SetActive(true);
+        }
+
+        if (TeleporterShow != null)
+        {
+            TeleporterShow.SetActive(true);
+        }
+
+        if (TeleporterHide != null)
+        {
+            TeleporterHide.SetActive(true);
+        }
+    }
+}
